356d263502185e61c364abe5ab086d9c7bfadb1a
EmIT <Emad.Armoun@gmail.com>
2017-09-03 08:46:58 +0430

    حذف توابع اضافه و ادغام موارد قابل ادغام کامنت گذاری توابع تکمیل تابع ساخت کیبورد اعداد سپردن ساخت دکمه بازگشت به تابع داخلی

--- EmITBotNet/KeyboardGenerator.cs
+++ EmITBotNet/KeyboardGenerator.cs
@@ -5,13 +5,12 @@ using Telegram.Bot.Types.ReplyMarkups;
 
 namespace ir.EmIT.EmITBotNet
 {
-    //todo کامنت گذاری کلاس
     //todo حذف توابع اضافه و ادغام موارد قابل ادغام
     //todo راست به چپ و برعکس
 
     public class KeyboardGenerator
     {
-        public static InlineKeyboardMarkup makeVerticalKeyboard(Dictionary<int, string> menu)
+        /*public static InlineKeyboardMarkup makeVerticalKeyboard(Dictionary<int, string> menu)
         {
             InlineKeyboardCallbackButton[][] keyboard = new InlineKeyboardCallbackButton[menu.Count][];
             for (int i = 0; i < menu.Count; i++)
@@ -31,8 +30,13 @@ namespace ir.EmIT.EmITBotNet
                 keyboard[i] = new InlineKeyboardCallbackButton(menu[i][1].ToString(), menu[i][0].ToString());
             }
             return new InlineKeyboardMarkup(keyboard);
-        }
+        }*/
 
+        /// <summary>
+        /// ساخت کیبورد عمودی از متن ها و داده های منو
+        /// </summary>
+        /// <param name="menu">منوی دوبعدی موردنظر از متن ها و داده ها</param>
+        /// <returns>کیبورد براساس منو</returns>
         public static InlineKeyboardMarkup makeVerticalKeyboard(string[][] menu)
         {
             InlineKeyboardCallbackButton[][] keyboard = new InlineKeyboardCallbackButton[menu.Length][];
@@ -45,6 +49,11 @@ namespace ir.EmIT.EmITBotNet
             return new InlineKeyboardMarkup(keyboard);
         }
 
+        /// <summary>
+        /// ساخت کیبورد افقی از متن ها و داده های منو
+        /// </summary>
+        /// <param name="menu">منوی دوبعدی موردنظر از متن ها و داده ها</param>
+        /// <returns>کیبورد براساس منو</returns>
         public static InlineKeyboardMarkup makeHorizontalKeyboard(string[][] menu)
         {
             InlineKeyboardCallbackButton[] keyboard = new InlineKeyboardCallbackButton[menu.Length];
@@ -55,6 +64,11 @@ namespace ir.EmIT.EmITBotNet
             return new InlineKeyboardMarkup(keyboard);
         }
 
+        /// <summary>
+        /// ساخت کیبورد عمودی از متن های منو
+        /// </summary>
+        /// <param name="menu">منوی موردنظر</param>
+        /// <returns>کیبورد براساس منو</returns>
         public static InlineKeyboardMarkup makeVerticalKeyboard(string[] menu)
         {
             InlineKeyboardCallbackButton[][] keyboard = new InlineKeyboardCallbackButton[menu.Length][];
@@ -67,6 +81,11 @@ namespace ir.EmIT.EmITBotNet
             return new InlineKeyboardMarkup(keyboard);
         }
 
+        /// <summary>
+        /// ساخت کیبورد افقی از متن های منو
+        /// </summary>
+        /// <param name="menu">منوی موردنظر</param>
+        /// <returns>کیبورد براساس منو</returns>
         public static InlineKeyboardMarkup makeHorizontalKeyboard(string[] menu)
         {
             InlineKeyboardCallbackButton[] keyboard = new InlineKeyboardCallbackButton[menu.Length];
@@ -123,14 +142,15 @@ namespace ir.EmIT.EmITBotNet
         /// <param name="textArr">آرایه شامل متن دکمه ها</param>
         /// <param name="columnCount">تعداد ستون موردنیاز در هر ستون</param>
         /// <param name="hasReturnBtn">آیا دکمه بازگشت هم باشد؟</param>
+        /// <param name="dataArr">آرایه شامل داده های متناظر دکمه ها</param>
         /// <returns>کیبورد تولید شده</returns>
         public static InlineKeyboardMarkup makeKeyboard(string[] textArr, int columnCount, bool hasReturnBtn, string[] dataArr = null)
         {
             int rowCount = (int)Math.Ceiling((double)textArr.Length / columnCount);
-            InlineKeyboardCallbackButton[][] keyboard = new InlineKeyboardCallbackButton[rowCount][];
+            InlineKeyboardCallbackButton[][] keyboard = new InlineKeyboardCallbackButton[rowCount + (hasReturnBtn ? 1 : 0)][];
             for (int i = 0; i < rowCount; i++)
             {
-                if (i == rowCount - 1 && hasReturnBtn)
+                if (i == rowCount - 1 && textArr.Length % columnCount != 0)
                     keyboard[i] = new InlineKeyboardCallbackButton[textArr.Length % columnCount];
                 else
                     keyboard[i] = new InlineKeyboardCallbackButton[columnCount];
@@ -141,11 +161,19 @@ namespace ir.EmIT.EmITBotNet
             }
 
             if (hasReturnBtn)
-                keyboard[rowCount-1][0] = new InlineKeyboardCallbackButton(textArr[textArr.Length - 1], "0");
+            {
+                keyboard[rowCount] = new InlineKeyboardCallbackButton[1];
+                keyboard[rowCount][0] = new InlineKeyboardCallbackButton("بازگشت \U000021A9", "0");
+            }
 
             return new InlineKeyboardMarkup(keyboard);
         }
 
+        /// <summary>
+        /// ساخت کیبورد شامل ماه های شمسی
+        /// </summary>
+        /// <param name="defaltMonth">ماه پیش فرض</param>
+        /// <returns>کیبورد ماه های شمسی</returns>
         public static InlineKeyboardMarkup makeMonthKeyboard(int defaltMonth = -1)
         {
             string[] monthArr = new string[12];
@@ -156,6 +184,11 @@ namespace ir.EmIT.EmITBotNet
             return makeKeyboard(monthArr, 3, false);
         }
 
+        /// <summary>
+        /// ساخت کیبورد شامل سال ها
+        /// </summary>
+        /// <param name="defaultYear">سال پیش فرض</param>
+        /// <returns>کیبورد سال ها</returns>
         public static InlineKeyboardMarkup makeYearKeyboard(int defaultYear = -1)
         {
             string[] yearTextArr = new string[10];
@@ -168,19 +201,28 @@ namespace ir.EmIT.EmITBotNet
             return makeKeyboard(yearTextArr, 5, false, yearDataArr);
         }
 
-        public static InlineKeyboardMarkup makeNumberMatrixKeyboard(int maxNumber, int columnCount = 5, bool hasReturnBtn = false, int defaultNumber = -1)
+        /// <summary>
+        /// ساخت کیبورد شامل سری اعداد
+        /// </summary>
+        /// <param name="fromNumber">از شماره</param>
+        /// <param name="toNumber">تا شماره</param>
+        /// <param name="columnCount">تعداد شماره در هر سطر</param>
+        /// <param name="hasReturnBtn">داشتن دکمه بازگشت</param>
+        /// <param name="defaultNumber">شماره پیش فرض</param>
+        /// <returns>کیبورد ماتریسی شامل بازه اعداد</returns>
+        public static InlineKeyboardMarkup makeNumberMatrixKeyboard(int fromNumber, int toNumber, int columnCount = 5, bool hasReturnBtn = false, int defaultNumber = -1)
         {
-            int roundMaxNumber = maxNumber;
-            if (maxNumber % columnCount != 0)
-                roundMaxNumber += (columnCount - (maxNumber % columnCount));
-            string[] numberArr = new string[roundMaxNumber + (hasReturnBtn ? 1 : 0)];
-            string[] dataArr = new string[roundMaxNumber + (hasReturnBtn ? 1 : 0)];
-            for (int i = 0; i < roundMaxNumber; i++)
+            int roundMaxNumber = toNumber;
+            if ((toNumber-fromNumber) % columnCount != 0)
+                roundMaxNumber += (columnCount - ((toNumber-fromNumber) % columnCount));
+            string[] numberArr = new string[roundMaxNumber - fromNumber];
+            string[] dataArr = new string[roundMaxNumber - fromNumber];
+            for (int i = 0; i < roundMaxNumber - fromNumber; i++)
             {
-                if (i < maxNumber)
+                if (i < toNumber)
                 {
-                    numberArr[i] = ((i + 1) + (i == defaultNumber - 1 ? " \U00002705" : "")).ToString();
-                    dataArr[i] = (i + 1).ToString();
+                    numberArr[i] = ((i + fromNumber) + (i + fromNumber == defaultNumber ? " \U00002705" : "")).ToString();
+                    dataArr[i] = (i + fromNumber).ToString();
                 }
                 else
                 {
@@ -188,12 +230,6 @@ namespace ir.EmIT.EmITBotNet
                     dataArr[i] = "-1";
                 }
             }
-            if (hasReturnBtn)
-            {
-                numberArr[roundMaxNumber] = "بازگشت \U000021A9";
-                dataArr[roundMaxNumber] = "0";
-            }
-            //todo سپردن ساخت دکمه بازگشت به تابع زیر
             return makeKeyboard(numberArr, columnCount, hasReturnBtn, dataArr);
         }
     }
